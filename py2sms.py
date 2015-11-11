ab3e1d721367f84b112dcb896e4433b357bf0f1a
metracy <matthew.etracy@gmail.com>
2015-11-11 16:56:36 -0500

    Update py2sms.py
    changed provider to sinch and inplemented the subprocess.call.  Learned about the pitfalls of needing to use backslashes in curl code.
    Fking a.

--- py2sms.py
+++ py2sms.py
@@ -1,19 +1,24 @@
-#  _____   __     __  ___     _____   __  __    _____ 
-# |  __ \  \ \   / / |__ \   / ____| |  \/  |  / ____|
-# | |__) |  \ \_/ /     ) | | (___   | \  / | | (___  
-# |  ___/    \   /     / /   \___ \  | |\/| |  \___ \ 
-# | |         | |     / /_   ____) | | |  | |  ____) |
-# |_|         |_|    |____| |_____/  |_|  |_| |_____/ 
-#                                                     
-                                                   
+'''
+d8888b. db    db .d888b. .d8888. .88b  d88. .d8888. 
+88  `8D `8b  d8' VP  `8D 88'  YP 88'YbdP`88 88'  YP 
+88oodD'  `8bd8'     odD' `8bo.   88  88  88 `8bo.   
+88~~~      88     .88'     `Y8b. 88  88  88   `Y8b. 
+88         88    j88.    db   8D 88  88  88 db   8D 
+88         YP    888888D `8888Y' YP  YP  YP `8888Y'  
+'''                                                   
 
-import subprocess
-import os
+import subprocess as sp
+#YOU WILL NEED TO CHANGE THE BELOW TO YOUR KEY AND YOUR SECRET AFTER YOU REGISTER WITH SINCH.COM
+key = '549e8c36-0ff9-49dd-87e8-02cbac532c3a' #your key here
+secret = 'EEHQnEBLtUGwzJdf+OWA1w==' #your secret here
+
+sysmsg = ['curl','--user','key:secret','--data','msg','-H','\'Content-Type: application/json\'','phoneurl']
+sysmsg[2] = '\"application\\%s:%s\"' % (key, secret)
 
 def sms(pnumber,msg):
-    sysmsg = 'curl http://textbelt.com/text -d number=' + str(pnumber) + ' -d ' + '\"message=' + str(msg) + '\"'
-    os.system(sysmsg)
-    #subprocess.call(sysmsg.split(' '))
+    sysmsg[4] = '\'{\"message\":\"%s\"}\'' % (str(msg))
+    sysmsg[7] = 'https://messagingapi.sinch.com/v1/sms/%s' % (str(pnumber))
+    sp.call(sysmsg)
     ##subprocess.check_output returns a string of output of the command too
     ##useful if you want to check the value returned to verify if the message sent
     #output = subprocess.check_output(sysmsg.split(' '))
@@ -21,7 +26,6 @@ def sms(pnumber,msg):
     #    print("Message: " + message + " failed to send to " + str(pnumber))
     #elif 'false' in output.lower():
     #    print("Message: " + message + " failed to send to " + str(pnumber))
-    #else:
     print("Message: " + msg + " sent to phone number: " + str(pnumber))
 
 # To send a text message call the function with
